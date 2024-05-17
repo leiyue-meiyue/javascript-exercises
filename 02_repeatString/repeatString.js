5b1d12cd90e7b1c9dbc6df4cf5ee86ced3c10ccd
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-17 00:01:51 -0400

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string,num) {
+    let text = string;
+    for (let i = 1; i < num; i++) {
+        text += string;
+    }
 
+    if(num == 0) {
+        text = '';
+    } else if (num < 0) {
+        text = 'ERROR';
+    }
+    return text;
 };
 
 // Do not edit below this line
