4cab976546828481e39cf9ceea20f79cb2ed1c09
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-17 00:45:01 -0400

    Complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+  let result = '';
+
+  let i = 0;
+  let letter = '';
+  let strLen = string.length;
+
+  while (i < strLen) {
+    letter = string.slice(-1);
+    result += letter;
+    string = string.slice(0, -1);
+    i++;
+  }
+
+  return result;
 };
 
 // Do not edit below this line
