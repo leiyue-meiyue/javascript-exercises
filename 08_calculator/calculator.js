f191203aceac28bcbb67f03973c9d4687e68db23
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-20 02:11:07 -0400

    Complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let result = 0;
+  array.forEach(num => result += num);
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  array.forEach(num => result *= num);
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  let result = x;
+  if (x == 0) return 1
+
+	for (let i = 1; i < x; i++) {
+    result *= (x - i)
+  }
+  return result
 };
 
 // Do not edit below this line
