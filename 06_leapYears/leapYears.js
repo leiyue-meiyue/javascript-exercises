31c33597f06b85615637b4ff5ae9e95a5bab33a1
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-20 01:21:15 -0400

    Complete 06_leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let cond = false;
 
+  // check if divisible by 4
+  if (year % 4 == 0) cond = true;
+  // if divisible by 100, check if divisible by 400
+  if (year % 100 == 0) {
+    cond = false
+    if (year % 400 == 0) cond = true;
+  }
+
+  return cond
 };
 
 // Do not edit below this line
