f365d4dc50a7770ae8fe69c7461d94af2cd44c97
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-20 01:45:20 -0400

    Complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+  let result = ((x - 32) * 5/9);
+  result = Math.round(result * 10) / 10;
+  result = parseFloat(result.toFixed(1));
+  return result
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(x) {
+  let result = (x * 9/5 + 32);
+  result = Math.round(result * 10) / 10;
+  result = parseFloat(result.toFixed(1));
+  return result
 };
 
 // Do not edit below this line
