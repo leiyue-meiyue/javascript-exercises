2fa6c08e2d54b40c846b6b9b504ae84507143b38
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-23 01:04:21 -0400

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  if (number < 0) return "OOPS"
+  if (number == 0) return 0
+  let sum = 1; 
+  if (number == 1 | number == 2) return sum
+  // calculate the fibonacci sequence up to the number
+  let num1 = 1;
+  let num2 = 1;
+  for (let i = 0; i < (number - 2); i++) {
+    sum = num1 + num2;
+    num1 = num2;
+    num2 = sum;
+  }
 
+  return sum
 };
 
 // Do not edit below this line
