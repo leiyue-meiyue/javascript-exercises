9099aaddcad47412527fdb34bc05cb6c1b854e94
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-23 00:44:00 -0400

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  // remove punctuation
+  string = string.replace(/[^\w\s\']|_/g, "");
+  // all lower case
+  string = string.toLowerCase();
+  // remove whitespace
+  string = string.replace(/\s/g, "");
+  // reverse the string
+  let reverse = string.split("").reverse().join("");
+  // see if it is the same as the original
+  // return true if so, false if not
+  return reverse == string
 };
 
 // Do not edit below this line
