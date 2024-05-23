55eb29bac4fbb551f8011c7760dfd166eac1e142
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-05-23 01:46:47 -0400

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+  let ages = [];
+  // loop through the array
+  for (let i = 0; i < array.length; i++) {
+    // calculate the age
+    let birth = array[i].yearOfBirth;
+    let death = array[i].yearOfDeath;
+    // deal with not dead people
+    if (typeof death != "number") {
+      death = new Date().getFullYear();
+    } 
+    let age = death - birth;
+    
+    // put that in an array probably
+    ages.push(age);
+  }
 
+  // find the index of the highest number?
+  let indexOfHighest = ages.reduce((maxIndex, elem, i, ages) =>  
+  elem > ages[maxIndex] ? i : maxIndex, 0);                      //figure this out later
+  
+  // get the person at that index
+  return array[indexOfHighest]
 };
 
 // Do not edit below this line
