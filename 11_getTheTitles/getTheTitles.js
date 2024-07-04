963c681bbd0d80a8704939226345df8740b724f5
leiyue <157659887+leiyue-meiyue@users.noreply.github.com>
2024-07-04 02:58:00 -0400

    Update 11_getTheTitles with new solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,10 +1,18 @@
-const getTheTitles = function(array) {
+// const getTheTitles = function(array) {
+//   let titles = [];
+//   for (let i = 0; i < array.length; i++) {
+//     titles.push(array[i].title);
+//   }
+//   return titles
+// };
+
+const getTheTitles = function(arr) {
   let titles = [];
-  for (let i = 0; i < array.length; i++) {
-    titles.push(array[i].title);
-  }
+  arr.map(book => (titles.push(book.title)));
   return titles
-};
+}
+
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
